573124cd067f7124911be076bb9a51878ba8a25f
dave0xE7 <redcorwp@gmail.com>
2022-08-06 03:04:36 +0000

    data

--- init.sh
+++ init.sh
@@ -15,3 +15,19 @@ fi
 
 echo $PLUG
 echo $PLUG_ID
+
+
+if ! [ -d $PLUG/plug_data ]; then
+    cd $PLUG
+    git clone git@github.com:dave0xE7/plug_data.git
+fi
+
+cd $PLUG/plug_data
+mkdir -vp hosts
+mkdir -vp hosts/$PLUG_ID
+ListBlockDevices | JsonPipe > hosts/$PLUG_ID/blockdevices
+GetSystemInfo | JsonPipe > hosts/$PLUG_ID/systeminfo
+
+git add hosts/$PLUG_ID
+git commit -m "added host $PLUG_ID"
+git push
